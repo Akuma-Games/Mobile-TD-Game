f04c293b08921fc9a83e3dcabd7e55f9714b82f4
xFluke <mikoman1124@gmail.com>
2021-01-30 11:58:07 -0500

    Added MainMenu and Gameplay Scene, with buttons to switch between scenes

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public void PlayAgain() {
+        SceneManager.LoadScene("GameplayScene");
+    }
+
+    public void ReturnToMainMenu() {
+        SceneManager.LoadScene("MainMenuScene");
+    }
+}
