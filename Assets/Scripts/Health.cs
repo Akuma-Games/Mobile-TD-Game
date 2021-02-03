d1c24825d6380195ca1ad465dd3883653879b7b6
Tariv093 <tariv093@gmail.com>
2021-02-03 15:02:38 -0500

    Health Bars Made
    Current pressing spacebar will reduce all hp of units down. just for testing purposes. Health bars should work otherwise

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth = 100;
+    private int currentHealth;
+    //public delegate void OnHealthChanged();
+    public event UnityAction<float> OnHealthChanged = delegate { } ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void ChangeHP(int amt)
+    {
+        currentHealth += amt;
+        float currentHPPct = (float)currentHealth / (float)maxHealth;
+        OnHealthChanged(currentHPPct);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        { ChangeHP(-10); }
+    }
+}
+
