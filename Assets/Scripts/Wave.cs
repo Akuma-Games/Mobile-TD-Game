e52b8dc56f4e18b3982584d6f0c423bc22a10c4f
xFluke <mikoman1124@gmail.com>
2021-02-19 18:09:34 -0500

    Functioning Wave Spawner

--- Assets/Scripts/Wave.cs
+++ Assets/Scripts/Wave.cs
@@ -5,7 +5,21 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Wave", menuName = "ScriptableObjects/Wave", order = 1)]
 public class Wave : ScriptableObject
 {
-    [SerializeField] WaveBlueprint[] enemyWave;
+    [SerializeField] WaveBlueprint[] enemyGroups;
+
+    [SerializeField] int currentGroupIndex = 0;
+
+    public WaveBlueprint GetGroupAtIndex(int i) {
+        return enemyGroups[i];
+    }
+
+    public int GetNumberOfGroups() {
+        return enemyGroups.Length;
+    }
+
+    public void FinishedSpawningGroup() {
+        currentGroupIndex++;
+    }
 }
 
 [System.Serializable]
@@ -14,8 +28,11 @@ public class WaveBlueprint
     [SerializeField] GameObject enemyPrefab;
     [SerializeField] int enemyCount;
     [SerializeField] float enemyRate;
-    [SerializeField] float cooldownForNextWave;
+    [SerializeField] float cooldownForNextGroup;
 
+    public GameObject EnemyPrefab {  get { return enemyPrefab; } }
     public int EnemyCount { get { return enemyCount; } }
     public float EnemyRate { get { return enemyRate; } }
+
+    public float CooldownForNextGroup {  get { return cooldownForNextGroup; } }
 }
--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -7,10 +7,38 @@ public class WaveSpawner : MonoBehaviour
     [SerializeField] Wave[] waves;
     [SerializeField] Transform spawnPosition;
 
+    [SerializeField] Wave currentWave;
+
     int waveIndex = 0;
 
     private void Start() {
+        currentWave = waves[waveIndex];
+        StartCoroutine(SpawnWave(currentWave));
+    }
+
+    private void Update() {
         
+    }
+
+    IEnumerator SpawnWave(Wave wave) {
+        int groupIndex = 0;
+
+        while (wave.GetNumberOfGroups() - groupIndex > 0) {
+
+            GameObject enemyToSpawn = wave.GetGroupAtIndex(groupIndex).EnemyPrefab;
+            int enemySpawned = 0;
+
+
+            while (wave.GetGroupAtIndex(groupIndex).EnemyCount - enemySpawned > 0) {
+                Instantiate(enemyToSpawn, spawnPosition.position, Quaternion.identity);
+                enemySpawned++;
+                yield return new WaitForSeconds(wave.GetGroupAtIndex(groupIndex).EnemyRate);
+            }
+
+            yield return new WaitForSeconds(wave.GetGroupAtIndex(groupIndex).CooldownForNextGroup);
+            groupIndex++;
+            
+        }
     }
 
 }
